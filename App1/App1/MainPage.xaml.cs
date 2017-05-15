cbd854677b6d50d668c43cb2708efeb369fdb6b7
Lach, Marcin (Ext) <marcin.lach@itmagination.pl>
2017-05-15 15:09:51 +0200

    Label with TapGestureRecognizer treats IsEnabled differently

---
+++ App1/App1/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitiallyTappableViewModel = new ViewModel(true);
+            InitiallyNotTappableViewModel = new ViewModel(false);
+            InitializeComponent();
+        }
+
+        public ViewModel InitiallyTappableViewModel { get; set; }
+        public ViewModel InitiallyNotTappableViewModel { get; set; }
+    }
+}
---
+++ App1/App1/ViewModel.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using App1.Annotations;
+using Xamarin.Forms;
+
+namespace App1
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        public ViewModel(bool isInitiallyTapable)
+        {
+            ToggleIsLabelTapableCommand = new Command(ToggleIsLabelTapable);
+            ChangeSlaveTextCommand = new Command(TapLabel);
+            _isLabelTapable = isInitiallyTapable;
+        }
+
+        public ICommand ToggleIsLabelTapableCommand { get; }
+        public ICommand ChangeSlaveTextCommand { get; }
+        
+        private bool _isLabelTapable;
+        public bool IsTapable
+        {
+            get { return _isLabelTapable; }
+            set
+            {
+                if (value != _isLabelTapable)
+                {
+                    _isLabelTapable = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        
+        private string _slaveLabelText = "Slave";
+        public string SlaveLabelText
+        {
+            get { return _slaveLabelText; }
+            set
+            {
+                if (_slaveLabelText != value)
+                {
+                    _slaveLabelText = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void TapLabel()
+        {
+            SlaveLabelText = DateTime.Now.ToString();
+        }
+
+        private void ToggleIsLabelTapable()
+        {
+            IsTapable = !IsTapable;
+        }
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
